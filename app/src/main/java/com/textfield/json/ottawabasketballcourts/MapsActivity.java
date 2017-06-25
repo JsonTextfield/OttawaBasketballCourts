488af49495fffa3707f2f7a57c53401f29ac4922
Jason <tehblackboy@gmail.com>
2017-06-25 06:35:59 -0400

    removing repeated code, going to work on showing/hiding markers

--- app/src/main/java/com/textfield/json/ottawabasketballcourts/MapsActivity.java
+++ app/src/main/java/com/textfield/json/ottawabasketballcourts/MapsActivity.java
@@ -1,39 +1,37 @@
 package com.textfield.json.ottawabasketballcourts;
 
 import android.Manifest;
-import android.content.DialogInterface;
-import android.content.pm.PackageManager;
 import android.database.Cursor;
-import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RadioGroup;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.textfield.json.ottawabasketballcourts.util.DB;
 
 import java.util.ArrayList;
 
+import info.hoang8f.android.segmented.SegmentedGroup;
+
 import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
-    private boolean[] courtTypes = {true, true};
-
-    ArrayList<Marker> full = new ArrayList<>();
-    ArrayList<Marker> half = new ArrayList<>();
+    ArrayList<Marker> markers = new ArrayList<>();
+    ArrayList<Court> courts = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,7 +52,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_maps, menu);
+        //getMenuInflater().inflate(R.menu.menu_maps, menu);
         return true;
     }
 
@@ -65,7 +63,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         //noinspection SimplifiableIfStatement
-        if (id == R.id.filter) {
+        /*if (id == R.id.filter) {
             String[] items = getResources().getStringArray(R.array.types);
             AlertDialog dialog = new AlertDialog.Builder(this, R.style.MyAlertDialogStyle)
                     .setTitle(getResources().getString(R.string.court_types))
@@ -91,7 +89,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                     }).create();
             dialog.show();
             return true;
-        }
+        }*/
 
         return super.onOptionsItemSelected(item);
     }
@@ -105,31 +103,46 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+    public void loadMarkers(final GoogleMap googleMap, String type) {
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        int padding = 10; // offset from edges of the map in pixels
+
+        if (type.equals("all")) {
+            for (Marker m : markers) {
+                m.setVisible(true);
+                builder.include(m.getPosition());
+            }
+        } else {
+            for (int i = 0; i < courts.size(); i++) {
+                if (courts.get(i).getType().equals(type)) {
+                    markers.get(i).setVisible(true);
+                    builder.include(markers.get(i).getPosition());
+                } else {
+                    markers.get(i).setVisible(false);
+                }
+            }
+        }
+        LatLngBounds bounds = builder.build();
+        final CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, padding);
+        googleMap.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
+            @Override
+            public void onMapLoaded() {
+                googleMap.animateCamera(cu);
+            }
+        });
+    }
+
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(final GoogleMap googleMap) {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
         }
 
-        LatLng me;
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_CALENDAR) == PERMISSION_GRANTED) {
-            googleMap.setMyLocationEnabled(true);
-            LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-            Location myself = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-            if(myself == null){
-                myself = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            }
-            try {
-                me = new LatLng(myself.getLatitude(), myself.getLongitude());
-            } catch (NullPointerException x) {
-                me = new LatLng(45.38, -75.69);
-            }
-        } else {
-            me = new LatLng(45.38, -75.69);
-        }
         googleMap.clear();
+        googleMap.setMyLocationEnabled(true);
 
-        DB db = new DB(this);
+        DB db = new DB(MapsActivity.this);
+
         db.createDatabase();
         db.open();
         Cursor cursor = db.runQuery("select * from courts;");
@@ -137,22 +150,34 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             Court c = new Court(cursor.getString(cursor.getColumnIndex("courttype")),
                     cursor.getString(cursor.getColumnIndex("name")),
                     cursor.getDouble(cursor.getColumnIndex("latitude")),
-                    cursor.getDouble(cursor.getColumnIndex("longitude")), cursor.getInt(cursor.getColumnIndex("id")));
-            if (c.getType().equals("full")) {
-                full.add(googleMap.addMarker(new MarkerOptions().position(c.getLocation()).icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher)).title(String.format("%s - %s", c.getName(), c.getType()))));
-            } else {
-                half.add(googleMap.addMarker(new MarkerOptions().position(c.getLocation()).icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher)).title(String.format("%s - %s", c.getName(), c.getType()))));
-            }
+                    cursor.getDouble(cursor.getColumnIndex("longitude")),
+                    cursor.getInt(cursor.getColumnIndex("id")));
+
+            courts.add(c);
+            Marker marker = googleMap.addMarker(new MarkerOptions().position(c.getLocation())
+                    .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher))
+                    .title(String.format("%s (%s)", c.getName(), c.getType())));
+            marker.setTag(c);
+            markers.add(marker);
         }
         while (cursor.moveToNext());
         db.close();
-        CameraPosition cameraPosition = new CameraPosition.Builder()
-                .target(me)      // Sets the center of the map to location user
-                .zoom(13)        // Sets the zoom
-                .bearing(-30)    // Sets the orientation of the camera to east
-                .build();
-        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
+        SegmentedGroup segmented = (SegmentedGroup) findViewById(R.id.segmented);
+        segmented.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+                switch (checkedId) {
+                    case R.id.full:
+                        loadMarkers(googleMap, "full");
+                    case R.id.all:
+                        loadMarkers(googleMap, "all");
+                    case R.id.half:
+                        loadMarkers(googleMap, "half");
+                }
+            }
+        });
+        loadMarkers(googleMap, "all");
     }
 
 }
