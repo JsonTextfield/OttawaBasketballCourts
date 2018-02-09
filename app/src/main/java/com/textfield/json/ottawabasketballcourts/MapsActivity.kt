c229061d14a4de705741c8fb2c6b4c91539cf851
Jason <tehblackboy@gmail.com>
2018-02-08 20:11:16 -0500

    screenshots and maps fix

--- app/src/main/java/com/textfield/json/ottawabasketballcourts/MapsActivity.kt
+++ app/src/main/java/com/textfield/json/ottawabasketballcourts/MapsActivity.kt
@@ -29,6 +29,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
 
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 0)
+        }
         mapFragment.getMapAsync(this)
     }
 
@@ -70,12 +73,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 0)
-        }
 
         googleMap.clear()
-        googleMap.isMyLocationEnabled = true
 
         val db = DB(this@MapsActivity)
 
---
+++ readme.md
@@ -0,0 +1,4 @@
+##Ottawa Basketball Courts
+Basketball courts in Ottawa
+
+![alt text](https://github.com/JsonTextfield/BallCourts/blob/master/Screenshot_1518138506.png)
